63fe71b5dfbd9dea78be62081a5d6e366dee0c84
Jean-Luc Martin <jl.martin1990@gmail.com>
2016-02-22 21:50:14 -0500

    added comment explaining use of mongoose-hidden

--- server/src/models/queueEntry.js
+++ server/src/models/queueEntry.js
@@ -8,13 +8,16 @@ var db = require('./index');
 var queueEntrySchema = new Schema({
   username    : { type : String, required: true, index: true },
   queueOwner  : { type : String, required: true, index: true },
-  isWaiting   : { type: Boolean, default: true },
+  isWaiting   : { type : Boolean, default: true },
   createdAt   : { type : Date,  default: new Date() }
 });
 
 // This ensures that each combination of username and queueOwner are unique
 queueEntrySchema.index({username: 1, queueOwner: 1}, {unique: true});
 
+// This hides the database ID and documention version information
+// when querying this model so that we don't have to send that
+// information to the client.
 queueEntrySchema.plugin(mongooseHidden);
 
 module.exports = db.model('QueueEntry', queueEntrySchema);
